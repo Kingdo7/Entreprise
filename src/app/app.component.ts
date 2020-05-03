bbdbacc1584301dfaca96d67744d01b000808e9f
k.renoleau <killianrenoleau@gmail.com>
2020-05-03 13:22:36 +0200

    Bouton get liste individus + afficher name individus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { WebServiceService } from './web-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ProjetA';
+  title = 'Projet A';
+
+  posts: any;
+
+  constructor(private webService: WebServiceService) {}
+
+  ngOnInit() {}
+
+  ExecuteWs() {
+    this.webService.getPosts().subscribe((data) => {
+      this.posts = data;
+      this.posts.forEach((post) => {
+        console.log(`Element n°${post.id} :`);
+        console.log(post.name);
+      });
+    });
+  }
 }

949063da0d8ad07f82d7324db9f51dfb77b655be
Retro Gaming Test <kingdommangas@gmail.com>
2020-05-04 18:26:50 +0200

    Ajout composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './personne/personne.component';
 import { WebServiceService } from './web-service.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonneComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [WebServiceService],
   bootstrap: [AppComponent],
